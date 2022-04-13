2f5aaecc117bda90088203bf1cd46d50b80aa58d
maxiarru <maxi.arru@gmail.com>
2022-04-13 15:05:15 -0300

    Create Medicion.cs
    agregando clase medicion

---
+++ PrimerProyecto/Medicion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerProyecto
+{
+    class Medicion
+    {
+        string fechaHora;
+        string temperatura;
+        string humedad;
+        string codigo;
+        string estado;
+        DateTime fecha;
+
+        public Medicion(string campo1, string campo2, string campo3, string campo4, string campo5, DateTime campo6)
+        {
+            this.fechaHora = campo1;
+            this.temperatura = campo2;
+            this.humedad = campo3;
+            this.codigo = campo4;
+            this.estado = campo5;
+            this.fecha = campo6;
+        }
+        public string GetFechaHora()
+        {
+            return fechaHora;
+        }
+        public DateTime GetDateTime()
+        {
+            return fecha;
+        }
+        public string GetTemperatura()
+        {
+            return temperatura;
+        }
+        public string GetHumedad()
+        {
+            return humedad;
+        }
+        public string GetCodigo()
+        {
+            return codigo;
+        }
+        public string GetEstado()
+        {
+            return estado;
+        }
+    }
+}
